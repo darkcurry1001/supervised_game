9e69a2e1fcfefd5b44fc7b20d8de861c1d0acf1a
Simina <simina.petruta@gmail.com>
2023-12-11 12:47:22 +0100

    Animations + beginning text

---
+++ data/game_text.py
@@ -0,0 +1,5 @@
+game_text = {
+    "intro": [["In the realm of Lumina, where light and shadow ","have coexisted in harmony, a forgotten conflict stirs anew."],
+              ["The once revered Enlightened Sentinels, guardians of balance,", "have dwindled to legend."],
+              ["Now, as shadows cleverly masquerade as light,"," the delicate equilibrium is threatened."]]
+}
