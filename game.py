3986ffe8ffd819c0fdaaac15c240c9e6a01d9a3b
darkcurry1001 <76181165+darkcurry1001@users.noreply.github.com>
2024-01-15 14:32:50 +0100

    Update game.py

--- game.py
+++ game.py
@@ -148,7 +148,7 @@ class Game:
         elif self.level == 2:
             self.tilemap.load(f'map-big3.json')
 
-        # self.tilemap.load('map-debug.json')
+        #self.tilemap.load('map-debug.json')
 
         # create player, enemies, npcs and light entities from spawners (and cont of enemies)
         for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1), ('spawners', 2), ('spawners', 3), ]):
