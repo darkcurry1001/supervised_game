dab8d2a4978d35b0acaee509c9459ae06ba28a4f
Simina <simina.petruta@gmail.com>
2024-01-15 21:15:42 +0100

    Added totems, level 2 dialogue, modified level 3 map

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import math
 
 BASE_IMG_PATH = 'data/images/'
 
@@ -58,14 +59,50 @@ class DialogueHandler:
         screen_width = 1280
         screen_height = 960
         self.dialogue_active = False
-        self.dialogue_box_rect = pygame.Rect(50, screen_height - 150, screen_width - 100, 120)
+        self.dialogue_box_rect = pygame.Rect(50, screen_height - 170, screen_width - 100, 140)
         self.current_line_index = 0
         self.dialogue_lines = []
+        self.choices = []  # To store possible choices for the player
+        self.response = None
+        self.totemid_solved = []
 
     def start_dialogue(self, dialogue_lines):
+        self.choices = []
         self.dialogue_lines = npc_dialogue[dialogue_lines]
         self.current_line_index = 0
         self.dialogue_active = True
+
+    def start_totem_dialogue(self, dialogue_index):
+        """
+        Start the totem dialogue.
+        dialogue_data is expected to be a dict with question, options, and responses.
+        """
+        if dialogue_index in self.totemid_solved:
+            self.choices=[]
+            self.dialogue_lines = npc_dialogue["totem" + str(dialogue_index)]
+            self.current_line_index = 0
+            self.dialogue_active = True
+
+        else:
+            self.dialogue_lines = [totem_data[dialogue_index]['question']]
+            self.choices = totem_data[dialogue_index]['options']
+            self.correct = totem_data[dialogue_index]["correct"]
+            self.response = totem_data[dialogue_index]['response']
+            self.dialogue_active = True
+            self.current_line_index = 0
+
+    def handle_choice(self, choice, totemid):
+        """
+        Handle the player's choice and show the totem's response.
+        """
+
+        self.current_line_index += 1  # Move to the response part
+        self.dialogue_lines.append(self.response[choice])
+        if choice == self.correct:
+            self.totemid_solved.append(totemid)
+            return True
+        else:
+            return False
 
     def next_line(self):
         if self.current_line_index < len(self.dialogue_lines) - 1:
@@ -91,6 +128,15 @@ class DialogueHandler:
                 y_offset += text_surface.get_height() + 5
         else:
             self.screen.fill((0, 0, 0, 0), self.dialogue_box_rect)
+
+        if self.current_line_index == 0 and self.choices:
+            y_offset += text_surface.get_height() + 5  # Space after the question
+            for i, choice in enumerate(self.choices):
+                choice_text = f"{i + 1}. {choice}"
+                choice_surface = self.font.render(choice_text, True, (255, 255, 255))
+                screen.blit(choice_surface, (self.dialogue_box_rect.x + 20, self.dialogue_box_rect.y + y_offset))
+                y_offset += choice_surface.get_height() + 5
+
 
 
 def wrap_text(text, font, max_width):
@@ -127,7 +173,7 @@ class Codex:
         self.font = font
         self.screen = screen
         self.codex_active = False
-        self.pages = codex_pages[1]
+        self.pages = codex_pages[0]
         self.current_page = 0
         self.codex_rect = pygame.Rect(100, 50, screen.get_width() - 200, screen.get_height() - 100)
         self.book_icon = pygame.image.load('data/images/codex_icon.png').convert_alpha()
@@ -177,7 +223,7 @@ def render_multiline_text(surface, text, pos, font, color):
 
 
 codex_pages = {
-    1: ['''1.Supervised Learning newline A type of machine learning where a model is trained on labeled data 
+    0: ['''1.Supervised Learning newline A type of machine learning where a model is trained on labeled data 
     (data that is paired with the correct answer). The model learns to make predictions or decisions based on this data.
     newline -------------------------------------------------------------------------------------------------------------------- newline
     2.Data Collection newline The process of gathering information, in various forms, to be used for analysis. 
@@ -196,7 +242,7 @@ codex_pages = {
 
 
 npc_dialogue = {
-    1: ['''Eren: Welcome, my friend. I've been expecting you. Like me, you carry the potential of the Enlightened Sentinels. We are the last of our kind, but our mission is more vital than ever.''',
+    0: ['''Eren: Welcome, my friend. I've been expecting you. Like me, you carry the potential of the Enlightened Sentinels. We are the last of our kind, but our mission is more vital than ever.''',
         '''You: Eren, I've heard stories of the Sentinels, but I never imagined I'd be part of this legacy. What exactly must we do? ''' ,
         '''Eren: Our world is in turmoil. Entities of light and shadow are indistinguishable, with shadows lurking beneath false guises. 
         Our task is to train the Lens of Insight, an ancient tool that sees the truth hidden beneath appearances. ''',
@@ -225,7 +271,7 @@ npc_dialogue = {
         "You: Then I'll gather as many images as I can. We'll bring clarity to Lumina once again.",
         '''Eren: May the light of wisdom guide you. I'll await your return with the gathered data. Together, we will embark on the next phase of our mission.'''
         ],
-    2: [
+    1: [
         "Eren: Welcome back, traveler. Your journey through the Umbra Frontier was fruitful, I trust?",
         "You: It was challenging, but I've gathered many images. What's our next step?",
         '''Eren: Now, we must learn to distinguish light from shadow. The key lies in understanding the subtle characteristics of light entities.
@@ -233,11 +279,130 @@ npc_dialogue = {
         "You: How do we decipher this knowledge?",
         '''Eren: The totems will reveal their secrets, but only to those who have truly grasped the fundamentals of our quest. 
         You must first prove your understanding of what you've learned so far.''',
-        "You: A test, then? I'm ready."
+        "You: A test, then? I'm ready.",
         '''Eren:  Answer correctly, and the totems will unveil the traits for correctly labeling light entities.
-        Pay close attention to the details; they will be instrumental in training the Lens of Insight accurately.'''
+        Pay close attention to the details; they will be instrumental in training the Lens of Insight accurately.''',
         "You: Understood. I'll do my best to unlock the wisdom of these ancient guardians.",
         "Eren: I have no doubt in your abilities. Go forth, and may the light of knowledge guide you."
     ],
-    3: []
+    2: ["Hello","Hi"],
+    "totem0": ["Never forget, the eyes never lie: blue, black and green, those are the colors bestowed upon light entities, everything else is but a sham"],
+    "totem1": ["No matter the length the creatures of darkness go to, they will never fully appear of the light. Look for spots of darkness,a leg or maybe a hand, to tell them apart"],
+    "totem2": ["A missing or lacking aura around the entities is a dead giveaway for those posers belonging to the darkness"],
+    "totem3": ["I shall give you a helping hand: The aura of a light entity guides us on the right path. It shall never be dark."],
+    "totem4": ["bla4"],
+    "totem5": ["bla5"],
+    "totem6": ["bla6"],
+    "totem7": ["bla7"]
 }
+
+totem_data = {
+    0:{'question': "Let's see if you are worthy! Here's a small test: In supervised learning, the model is trained only on unlabeled data.",
+    'options': [
+        "Press 1: True",
+        "Press 2: False"
+    ],
+    'response': {
+        '2': "Not bad, young one! Never forget, the eyes never lie: blue, black and green, those are the colors bestowed upon light entities, everything else is but a sham",
+        '1': "I see you still have a long way to go..."
+        },
+    'correct': "2"
+
+    },
+    1:{'question': "You shall first answer my question: The process of data collection in machine learning involves gathering text and numbers only, not images, is that true?",
+    'options': [
+        "Option 1: True",
+        "Option 2: False"
+    ],
+    'response': {
+        '2': "That is correct! No matter the length the creatures of darkness go to, they will never fully appear of the light. Look for spots of darkness,a leg or maybe a hand, to tell them apart",
+        '1': "Wrong!"
+        },
+    'correct': "2"
+    },
+    2:{'question': "Answer this to show your understanding. Why is having a large dataset important in supervised learning?",
+    'options': [
+        "Option 1: To increase variety, which leads to better generalization and better predictions.",
+        "Option 2: To make the training longer and more complex."
+    ],
+    'response': {
+        '1': "Well done. A missing or lacking aura around the entities is a dead giveaway for those posers belonging to the darkness",
+        '2': "You shall ponder over that some more"
+        },
+    'correct': "1"
+    },
+    3:{'question': "Tell me, young one, what is labeled data in the context of supervised learning?",
+    'options': [
+        "Option 1: Data that is organized in alphabetical order.",
+        "Option 2: Data tagged with one or more labels identifying certain properties or classifications."
+    ],
+    'response': {
+        '1': "You still have a lot to learn...",
+        '2': "Impressive! I shall give you a helping hand: The aura of a light entity guides us on the right path. It shall never be dark."
+        },
+    'correct': "2"
+    },
+    4:{'question': "The totem asks you a question 4...",
+    'options': [
+        "Option 1: The first choice",
+        "Option 2: The second choice"
+    ],
+    'response': {
+        '1': "The totem's response to the first choice",
+        '2': "The totem's response to the second choice"
+        },
+    'correct': "1"
+    },
+    5:{'question': "The totem asks you a question 4...",
+    'options': [
+        "Option 1: The first choice",
+        "Option 2: The second choice"
+    ],
+    'response': {
+        '1': "The totem's response to the first choice",
+        '2': "The totem's response to the second choice"
+        },
+    'correct': "1"
+    },
+    6:{'question': "The totem asks you a question 4...",
+    'options': [
+        "Option 1: The first choice",
+        "Option 2: The second choice"
+    ],
+    'response': {
+        '1': "The totem's response to the first choice",
+        '2': "The totem's response to the second choice"
+        },
+    'correct': "1"
+    },
+    7:{'question': "The totem asks you a question 4...",
+    'options': [
+        "Option 1: The first choice",
+        "Option 2: The second choice"
+    ],
+    'response': {
+        '1': "The totem's response to the first choice",
+        '2': "The totem's response to the second choice"
+        },
+    'correct': "1"
+    }
+}
+
+def render_proximity(self, player_pos, screen, render_cam_offset):
+    distance = math.sqrt((self["pos"][0] - player_pos[0]) ** 2 + (self["pos"][1] - player_pos[1]) ** 2)
+    font = pygame.font.SysFont('Arial', 15)
+    if distance < 40:
+        self["dialogue"] = True
+        text_surface = font.render('Press N', True, (255, 255, 255))
+
+        # Translate the NPC's world position into camera-relative screen position
+        text_x = self["pos"][0] * 4 - render_cam_offset[0] * 4 + 30
+        text_y = self["pos"][1] * 4 - render_cam_offset[1] * 4 + 80  # Adjust the offset as needed
+
+        text_rect = text_surface.get_rect(center=(text_x, text_y))
+        screen.blit(text_surface, text_rect)
+    else:
+        self["dialogue"] = False
+
+
+
